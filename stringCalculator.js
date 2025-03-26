587f06b3db3ef60dc33d5a099034d378f5382e2e
milanpaladiya97 <milanpaladiya91@gmail.com>
2025-03-26 17:24:09 +0530

    feat: Support custom delimiters

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,7 +1,15 @@
 function add(numbers) {
     if (numbers === "") return 0;
 
-    const numArray = numbers.split(/,|\n/).map(num => parseInt(num, 10));
+    let delimiter = /,|\n/; // Default delimiters
+
+    if (numbers.startsWith("//")) {
+        const parts = numbers.split("\n");
+        delimiter = new RegExp(parts[0].slice(2)); // Extract delimiter
+        numbers = parts[1];
+    }
+
+    const numArray = numbers.split(delimiter).map(num => parseInt(num, 10));
     return numArray.reduce((sum, num) => sum + num, 0);
 }
 
