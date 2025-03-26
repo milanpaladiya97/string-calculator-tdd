d1a7d1af348220ab30ab3afca99325322a942a4a
milanpaladiya97 <milanpaladiya91@gmail.com>
2025-03-26 17:21:46 +0530

    feat: Handle two comma-separated numbers

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -3,3 +3,11 @@ const add = require("./stringCalculator");
 test("returns 0 for an empty string", () => {
     expect(add("")).toBe(0);
 });
+
+test("returns the number itself when a single number is passed", () => {
+    expect(add("5")).toBe(5);
+});
+
+test("returns sum when two numbers are passed", () => {
+    expect(add("1,2")).toBe(3);
+});
