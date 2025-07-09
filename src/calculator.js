273e2329415e6215bc9468c58d6f6c0f1a67f2e1
milanpaladiya97 <milanpaladiya91@gmail.com>
2025-07-09 12:55:04 +0530

    Refactor: Simplify add method to handle all cases with split

--- src/calculator.js
+++ src/calculator.js
@@ -3,9 +3,6 @@ class Calculator {
     if (numbers === '') {
       return 0;
     }
-    if (!numbers.includes(',')) {
-      return parseInt(numbers, 10);
-    }
     const numberArray = numbers.split(',').map(num => parseInt(num, 10));
     return numberArray.reduce((sum, num) => sum + num, 0);
   }
